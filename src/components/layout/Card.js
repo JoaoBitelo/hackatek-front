a47a6d3c4af8c446616ce01be43504e077e96285
Tiago Villa <tiago_villa@hotmail.com>
2021-07-20 20:27:37 -0300

    Adiciona pasta de content

--- src/components/layout/Card.js
+++ src/components/layout/Card.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import image from "../../content/images/Rectangle 5.png";
 
 function CardEvent() {
-  return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="" />
-      <Card.Body>
-        <Card.Title>Dia Feliz</Card.Title>
-        <Card.Text>Descrição </Card.Text>
-      </Card.Body>
-    </Card>
-  );
+    return (
+        <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+                <Card.Title>Dia Feliz</Card.Title>
+                <div className="heart-icon">
+                    <i class="far fa-heart"></i>
+                </div>
+                <div className="share-icon">
+                    <i class="fas fa-share-alt"></i>
+                </div>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default CardEvent;
