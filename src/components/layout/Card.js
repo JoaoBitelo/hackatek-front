3123c189d9d675633a2876cf5a1098fb291d5e2a
Joao Bitelo <joao.octavio.bitelo@gmail.com>
2021-07-21 20:38:27 -0300

    Usa fetch para renderizar

--- src/components/layout/Card.js
+++ src/components/layout/Card.js
@@ -4,13 +4,14 @@ import image from "../../content/images/Rectangle 5.png";
 
 function CardEvent(props) {
     const hasIcons = props.hasIcons;
+    console.log("item", props.item);
     return (
         <Card style={{ width: "14rem" }}>
             <Card.Img variant="top" src={image} />
             <Card.Body>
-                <Card.Title>Dia Feliz</Card.Title>
+                <Card.Title>{props.item.name}</Card.Title>
                 <Card.Text>
-                    {hasIcons ? (
+                    {hasIcons && (
                         <div className="icons">
                             <div className="icon">
                                 <i class="far fa-heart fa-2x"></i>
@@ -19,8 +20,6 @@ function CardEvent(props) {
                                 <i class="fas fa-share-alt fa-2x"></i>
                             </div>
                         </div>
-                    ) : (
-                        ""
                     )}
                 </Card.Text>
             </Card.Body>
--- src/components/layout/CardsContainer.js
+++ src/components/layout/CardsContainer.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import CardEvent from "./Card";
 
+const limitToReturn = 4;
+
+const fetchData = async (hasIcons) => {
+    const res = await fetch("http://localhost:3000/event?_limit=" + limitToReturn);
+    return res.json();
+};
+
 function CardsContainer(props) {
+    const [cards, setCards] = useState([]);
+    useEffect(() => {
+        fetchData(hasIcons).then((res) => {
+            setCards(res);
+        });
+    }, []);
+
     const hasIcons = props.hasIcons;
     return (
         <Container className="events-container">
             <h4>{props.title}</h4>
             <Row md={4} className="g-4">
-                {Array.from({ length: 4 }).map((_, idx) => (
+                {cards.map((item, idx) => (
                     <Col>
-                        <CardEvent hasIcons={hasIcons}></CardEvent>
+                        <CardEvent item={item} hasIcons={hasIcons}></CardEvent>
                     </Col>
                 ))}
             </Row>
