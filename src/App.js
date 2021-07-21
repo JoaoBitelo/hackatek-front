07ea661b6cd5bd7c4f6f5996387d3444883adad2
root <root@DESKTOP-5RP4UDS.localdomain>
2021-07-21 02:53:54 -0300

    add banner component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 
 import Header from "./components/layout/Header";
+import Banner from "./components/layout/Banner";
 import CardsContainer from "./components/layout/CardsContainer";
 
 function App() {
     return (
         <div className="App">
             <Header></Header>
+            <Banner></Banner>
             <CardsContainer
                 hasIcons={true}
                 title="Ações perto de você esse mês"
--- src/components/layout/Banner.js
+++ src/components/layout/Banner.js
@@ -1,8 +1,32 @@
 import React from "react";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, Container, Image, Row, Col } from "react-bootstrap";
+import image from "../../content/images/mulher_de_mascara_falando.png";
 
 function Banner() {
-  return <Jumbotron fluid></Jumbotron>;
+  return ( 
+    <Container className="event-page-container">
+      <Row>
+        <Col>
+          <Jumbotron fluid>
+            <div className="pages-path">
+              <p> Home {">"} Eventos e Ações Solidárias </p>
+            </div>
+            <div className="big-description">
+              <p> O Voluntariado na pandemia </p>
+            </div>
+            <div className="app-name">
+              <p> I care a lot. </p>
+            </div>
+          </Jumbotron>
+        </Col>
+        <Col>
+          <div className="container-2">
+            <Image src={ image } fluid/>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Banner;
