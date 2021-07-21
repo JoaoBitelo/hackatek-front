81a6c5016b498bc0825eef3c4107cab63beacc9f
Joao Bitelo <joao.octavio.bitelo@gmail.com>
2021-07-20 22:11:59 -0300

    Header pronto, mas sem design

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,10 +3,9 @@ import {
     Container,
     Navbar,
     Image,
-    Row,
     InputGroup,
     FormControl,
-    Col,
+    Nav,
 } from "react-bootstrap";
 import image from "../../content/images/circleLogo.png";
 import ButtonGeneric from "./Button";
@@ -15,46 +14,52 @@ function Header() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Row>
-                    <Col>
-                        <Image roundedCircle src={image} />
-                    </Col>
-                    <Col>
+                <Navbar.Brand href="#home">
+                    <Image roundedCircle src={image} />
+                </Navbar.Brand>
+                <Container>
+                    <Nav className="me-auto">
                         <InputGroup>
                             <FormControl
                                 placeholder="Pesquisar eventos e ações solidárias"
                                 aria-describedby="basic-addon2"
+                                style={{
+                                    borderWidth: 1,
+                                    borderColor: "#A992C5",
+                                }}
                             />
                         </InputGroup>
-                    </Col>
-                    <Col>
-                        <Row>
-                            <Col>
-                                <i
-                                    class="fas fa-map-marker-alt"
-                                    style={{
-                                        color: "#A992C5",
-                                        fontSize: "1.5rem",
-                                    }}
-                                ></i>
-                            </Col>
-                            <Col>
-                                <p
-                                    style={{
-                                        color: "black",
-                                        fontSize: "1.5rem",
-                                        textDecorationLine: "underline",
-                                    }}
-                                >
-                                    Porto Alegre
-                                </p>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col>
+                    </Nav>
+                </Container>
+
+                <Container>
+                    <Nav className="me-auto">
+                        <i
+                            class="fas fa-map-marker-alt"
+                            style={{
+                                color: "#A992C5",
+                                fontSize: "1rem",
+                                paddingRight: 10,
+                            }}
+                        ></i>
+
+                        <p
+                            style={{
+                                color: "#3A3A3A",
+                                fontSize: "1rem",
+                                textDecorationLine: "underline",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            Porto Alegre
+                        </p>
+                    </Nav>
+                </Container>
+                <Container style={{backgroundColor: "red"}}>
+                    <Nav className="me-auto">
                         <ButtonGeneric text={"Cadastre-se"} />
-                    </Col>
-                </Row>
+                    </Nav>
+                </Container>
             </Container>
         </Navbar>
     );
