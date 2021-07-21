ee5e13b9d18ac45223c9c2d176e2beb1f6160664
Tiago Villa <tiago_villa@hotmail.com>
2021-07-20 23:41:09 -0300

    Adiciona imagens; refatora navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -9,6 +9,7 @@ import {
 } from "react-bootstrap";
 import image from "../../content/images/circleLogo.png";
 import ButtonGeneric from "./Button";
+import { Button } from "react-bootstrap";
 
 function Header() {
     return (
@@ -17,49 +18,47 @@ function Header() {
                 <Navbar.Brand href="#home">
                     <Image roundedCircle src={image} />
                 </Navbar.Brand>
-                <Container>
-                    <Nav className="me-auto">
-                        <InputGroup>
-                            <FormControl
-                                placeholder="Pesquisar eventos e ações solidárias"
-                                aria-describedby="basic-addon2"
-                                style={{
-                                    borderWidth: 1,
-                                    borderColor: "#A992C5",
-                                }}
-                            />
-                        </InputGroup>
-                    </Nav>
-                </Container>
 
-                <Container>
-                    <Nav className="me-auto">
-                        <i
-                            class="fas fa-map-marker-alt"
+                <Nav className="me-auto">
+                    <InputGroup>
+                        <FormControl
+                            placeholder="Pesquisar eventos e ações solidárias"
+                            aria-describedby="basic-addon2"
                             style={{
-                                color: "#A992C5",
-                                fontSize: "1rem",
-                                paddingRight: 10,
+                                borderWidth: 1,
+                                borderColor: "#A992C5",
                             }}
-                        ></i>
+                        />
+                    </InputGroup>
+                </Nav>
 
-                        <p
-                            style={{
-                                color: "#3A3A3A",
-                                fontSize: "1rem",
-                                textDecorationLine: "underline",
-                                fontWeight: "bold",
-                            }}
-                        >
-                            Porto Alegre
-                        </p>
-                    </Nav>
-                </Container>
-                <Container style={{backgroundColor: "red"}}>
-                    <Nav className="me-auto">
-                        <ButtonGeneric text={"Cadastre-se"} />
-                    </Nav>
-                </Container>
+                <Nav className="me-auto">
+                    <i
+                        class="fas fa-map-marker-alt fa-2x"
+                        style={{
+                            color: "#A992C5",
+
+                            paddingRight: 10,
+                        }}
+                    ></i>
+
+                    <p
+                        style={{
+                            color: "#3A3A3A",
+                            fontSize: "1rem",
+                            textDecorationLine: "underline",
+                            fontWeight: "bold",
+                        }}
+                    >
+                        Porto Alegre
+                    </p>
+                </Nav>
+
+                <Nav className="me-auto">
+                    <Button size="lg" variant="primary">
+                        Cadastre-se
+                    </Button>
+                </Nav>
             </Container>
         </Navbar>
     );
